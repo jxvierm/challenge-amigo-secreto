0420889b7ae759e2dc118d6ff8d33ed709267533
jxvierm <jxviermxrk16@gmail.com>
2025-03-18 20:24:12 -0500

    validar una entrada

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,2 +1,18 @@
 //array para almacenar los nombres amigos 
 let amigos = [];
+
+/**
+ * Agrega un nuevo amigo al array de amigos.
+ * Valida que el nombre no esté vacío antes de agregarlo y actualiza la lista visual.
+ */
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+  
+    if (nombreAmigo.trim() === "") {
+      alert("Por favor, inserte un nombre");
+    } else {
+      amigos.push(nombreAmigo);
+      document.querySelector("#amigo").value = "";
+      mostrarListaAmigo();
+    }
+  }
